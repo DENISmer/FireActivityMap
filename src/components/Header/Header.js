e95734f4f4af794f350ff266950c4c5725592919
79963 <rediskink@inbox.ru>
2023-01-07 23:31:26 +0700

    добавил Header.js и Header.css(основная разметка + рамки) MainNavBar.js тут меню

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+export function Header(){
+    return(
+        <div >
+            <header className="Header">
+                <div>
+                    выплывающее меню
+                </div>
+                <div>
+                     вход
+                </div>
+                <div>
+                    контакты
+                </div>
+            </header>
+        </div>
+    );
+}
--- src/components/MainNavBar/MainNavBar.js
+++ src/components/MainNavBar/MainNavBar.js
@@ -1,17 +1,31 @@
 import React from "react";
 import './MainNavBar.css';
+import {useButton} from "@mui/base/ButtonUnstyled";
+import {ButtonUnstyled} from "@mui/base";
+import UnstyledButtonsSimple from "../buttons/materialui buttons";
 export function MainNavBar(){
     return(
         <div className="navBar">
-            <div className="#">
-                тут будет основная информация
+            <div className="navBarMainInstuments">
+                <h2>тут будет основная информация</h2>
             </div>
-            <div className="#">
-                <button>first</button>
+
+            <div className="navBarMainInstuments">
+                <h2>
+                    <UnstyledButtonsSimple />
+                    <br/>
+                    <UnstyledButtonsSimple />
+                    <br/>
+                    <UnstyledButtonsSimple />
+                    <button className='navBar_button'>first</button>
+                    <button className='navBar_button'>second</button>
+                    <button className='navBar_button'>third</button>
+                </h2>
             </div>
-            <div className="#">
-                дополнительные инстр
+            <div className="navBarMainInstuments">
+                <h2>доп информация</h2>
             </div>
         </div>
+
         );
 }
