f0b5f101b784dd42a9dbf0e41f4415cd5fc78d89
79963 <rediskink@inbox.ru>
2023-01-12 23:14:19 +0700

    Added: 1) validation on Signin.jsx 2) styled validation errors Signin.jsx + Signup.css 3) test fetch requests on https://jsonplaceholder.typicode.com/users

--- src/Login/Authorization/Signin.jsx
+++ src/Login/Authorization/Signin.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import '../Registration/Signup.css'
 
 export function Signin(){
     const navigate = useNavigate();
+    const [mail, setMail] = useState('');
+    const [password, setPassword] = useState('');
+    const [mailError, setMailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
+    // const [isLoading, setIsLoading] = useState(false);
+
+    const URL = "https://jsonplaceholder.typicode.com/users/";
+
+    const isValid = (): boolean =>{
+        let result = true;
+        setMailError("");
+        setPasswordError('');
+        if (mail.length < 6) {
+            console.log("Error")
+            setMailError("Слишком короткий логин");
+            result = false;
+        }
+        else if(!/[^a-zA-Z0-9]/.test(mail)){
+            setMailError('Недопустимые символы в логине');
+            result = false
+        }
+        if (password.length <=7) {
+            setPasswordError('Слишком короткий пароль!');
+            result = false
+        }
+        return result;
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        isValid() ? loginRequest(mail,password) : console.log("this data is not Valid");
+    }
+
+    const loginRequest = async (mail,password) => {
+        const response = await fetch(`${URL}/${1}`);
+
+        const data = await response.json();
+
+        if(response.status !== 200){
+            setMailError('Ошибка: Проверьте правильность введенных данных')
+            console.log(response);
+        }
+        else if (data.email === "Sincere@april.biz") {
+            data.toString();
+            console.log(data);
+            navigate('/Map');
+        }
+    }
     return<>
+        <form onSubmit={handleSubmit}>
         <div className='parent'>
             <img className='logos-img' src="https://img3.akspic.ru/crops/7/4/5/5/25547/25547-zemlya-noch-prostranstvo-vselennaya-kosmos-1920x1080.jpg"/>
             <div className='block'>
@@ -12,14 +62,17 @@ export function Signin(){
                     <h2>Авторизация</h2>
                 </div>
                 <div className='inputs_block'>
-                    <input placeholder='Почта'></input>
-                    <input type="password" placeholder='Пароль'></input>
+                    <input type="email" placeholder='Почта'onChange={e => setMail(e.target.value)} value={mail}></input>
+                    {mailError && <div className="error">{mailError}</div>}
+                    <input type="password" onChange={e => setPassword(e.target.value)} placeholder='Пароль'></input>
+                    {passwordError && <div className="error">{passwordError}</div>}
                 </div>
                 <div>
-                    <button onClick={()=> navigate('/Map')}>Войти</button>
+                    <button type={"submit"}>Войти</button>
                     <a href="" onClick={()=> navigate('/Signup')}>Нет аккаунта?</a>
                 </div>
             </div>
         </div>
+        </form>
     </>
 }
