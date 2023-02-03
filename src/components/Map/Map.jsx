48010410628fb6abb129e62db486e829fb2c4943
79963 <rediskink@inbox.ru>
2023-02-04 06:28:48 +0700

    Добавлен json с данными из FIRMS(MODIS) папка info вывод точек пожаров через панель смены слоя Информация о точке пожара(Время, Координаты,яркость)

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {GeoJSON, useMap, useMapEvent, useMapEvents} from "react-leaflet";
+import React, {useEffect, useRef, useState} from 'react';
+import {Circle, useMap, useMapEvent, useMapEvents} from "react-leaflet";
 import L from 'leaflet';
 import {
     MapContainer,
@@ -14,28 +14,51 @@ import './Map.css';
 import {MainNavBar} from "../MainNavBar/MainNavBar";
 import {Header} from "../Header/Header";
 import {MouseCoordinates} from "../Info/Coordinates";
-
-
+import nationalParks from '../Info/national-parks.json'
+import {GeoJSON} from 'react-leaflet/GeoJSON'
+//import axios from "axios";
 function GetIcon(_iconSize){
     return L.icon({
-        iconUrl: require("../../icons/marker.png"),
+        iconUrl: require("../../icons/red_dot_marker.png"),
         iconSize: [_iconSize]
     })
 }
 
 export default function MapComponent(){
-
     const {BaseLayer} = LayersControl;
+
     const center = [33.505, -0.09]
+
+    // const [geoJSON,setgeoJSON] = useState(null);
+    // const [reference,setReference] = useState(null);
+    //
+    // const getHospitals = async () => {
+    //     let hospitals = await axios.get(
+    //         "https://cartovis-server.herokuapp.com/hospitales"
+    //     );
+    //
+    //     let reference = React.createRef();
+    //
+    //     setgeoJSON(hospitals);
+    //     setReference(reference)
+    //     console.log("zbs")
+    // };
+
+            console.log(nationalParks.map(nat => (nat.latitude))
+            )
+
+
 
 
     return <>
-                <MapContainer minZoom={2.3} maxZoom={13} zoom={3} center={center} doubleClickZoom={false} maxBounds={[[-110,-170],[100,200]]} >
+                <MapContainer minZoom={2.3} maxZoom={13} zoom={3} center={center}  doubleClickZoom={false} maxBounds={[[-110,-170],[100,200]]} >
                     <ScaleControl position="topleft" />
                     <Header />
                     <MainNavBar />
                     <MouseCoordinates />
+                    <GeoJSON
 
+                    />
                     <LayersControl>
                         <BaseLayer name="Sattelite" checked={true} >
                             <TileLayer
@@ -49,12 +72,30 @@ export default function MapComponent(){
                                 url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
                             />
                         </BaseLayer>
+                        <LayersControl.Overlay checked name="Fires from FIRMS(MODIS)">
+                            <LayerGroup>
+                                {nationalParks.map(nat =>(
+                                    <Marker icon={GetIcon(5,5)}
+                                            key = {nat.frp}
+                                            position = {[nat.latitude,nat.longitude]}>
+                                        <Popup>
+                                            Время: {nat.acq_time}
+                                            <br/>
+                                            Координаты: {nat.latitude}, {nat.longitude}
+                                            <br/>
+                                            Яркость: {nat.brightness}
+                                            <br/>
+                                        </Popup>
+                                    </Marker>
+                                ))}</LayerGroup>
+                        </LayersControl.Overlay>
                         <BaseLayer name="ESRI" >
                             <LayerGroup>
                                 <TileLayer
                                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                     url={'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'}
                                 />
+
                                 <Marker position={[55,33]} icon={GetIcon(20,20)}>
                                     <Popup>
                                         yoooo
@@ -68,7 +109,6 @@ export default function MapComponent(){
                             text here
                         </Popup>
                     </Marker>
-                    <GeoJSON />
                 </MapContainer>
             </>
     }
