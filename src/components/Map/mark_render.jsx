a0fdd4f1d2491ade0ef535418c860158e31ef106
79963 <rediskink@inbox.ru>
2023-02-05 03:36:55 +0700

    Класетры в mark_render.jsx

--- src/components/Map/mark_render.jsx
+++ src/components/Map/mark_render.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import nationalParks from "../Info/national-parks.json";
 import L from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import {Tooltip} from "@mui/material";
 
 function GetIcon(_iconSize){
     return L.icon({
@@ -15,22 +16,28 @@ function GetIcon(_iconSize){
 export function Mark_render() {
 
     return(<>
-            <MarkerClusterGroup maxClusterRadius={20}>{nationalParks.map(nat =>(
-                <Marker icon={GetIcon(10,10)}
-                        key = {nat.frp}
-                        position = {[nat.latitude,nat.longitude]}>
-                    <Popup tittle={nat.acq_time}>
-                        Time: {nat.acq_time}
-                        <br/>
-                        Coordinates: {nat.latitude}, {nat.longitude}
-                        <br/>
-                        Brightness: {nat.brightness}
-                        <br/>
-                        Track: {nat.track}
-                        <br/>
-                        Scan: {nat.scan}
-                    </Popup>
-                </Marker>
+            <MarkerClusterGroup
+                //onClick={(e) => (<Popup> text here<Popup/>)}
+                spiderfyDistanceMultiplier={1}
+                maxClusterRadius={20}
+                singleMarkerMode={true}
+                >
+                {nationalParks.map(nat =>(
+                    <Marker icon={GetIcon(10,10)}
+                            key = {nat.frp}
+                            position = {[nat.latitude,nat.longitude]}>
+                        <Popup tittle={nat.acq_time}>
+                            Time: {nat.acq_time}
+                            <br/>
+                            Coordinates: {nat.latitude}, {nat.longitude}
+                            <br/>
+                            Brightness: {nat.brightness}
+                            <br/>
+                            Track: {nat.track}
+                            <br/>
+                            Scan: {nat.scan}
+                        </Popup>
+                    </Marker>
             ))}</MarkerClusterGroup>
     </>
     );
