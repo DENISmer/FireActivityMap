1878a9c108b3adf72a9642ae43b7148d64d850f8
rubinnaw <rubinnaw@gmail.com>
2023-05-08 22:04:10 +0700

    Попытка фикса settLements

--- src/components/Map/layers/localities.jsx
+++ src/components/Map/layers/localities.jsx
@@ -2,6 +2,7 @@ import {URL_FOR_COORDS} from '../../../config/config'
 import {Marker, Polyline, Popup} from "react-leaflet";
 import L from 'leaflet';
 import Icon from 'leaflet';
+import {localities} from "../../../data/coordinateFiles/localities";
 import {useEffect, useState} from "react";
 //import icon from '../../../icons/2x/outline_maps_home_work_black_24dp.png'
 
@@ -21,45 +22,53 @@ export function SettLements(){
     const [active, setActive] = useState(false);
 
 
-    const request = async() =>{
-
-        try {
-            return await require(`../../../${URL_FOR_COORDS.SETTLEMENTS}`)
-        }
-        catch (e) {
-            console.log(e)
-        }
-    }
-
-    reply = request()
-
-
-    useEffect(() => {
-        request().then(response =>{
-            setActive(true)
-            for (let index in response){
-                if (response.hasOwnProperty(index)){
-                    // console.log(reply[index].name + '\n', 'longitude: ' + reply[index].longitude + '\n', 'latitude: ' + reply[index].latitude)
-                    localSettLementArray.push({name: response[index].name, longitude: response[index].longitude,latitude: response[index].latitude})
-                }
-            }
-            setLementArray(localSettLementArray)
-            // console.log('useEffect: ' + settLementArray)
-            // console.log(localSettLementArray[0].name)
-        })
-    },[])
+    // const request = async() =>{
+    //
+    //     try {
+    //         return await require(`../../../${URL_FOR_COORDS.SETTLEMENTS}`)
+    //     }
+    //     catch (e) {
+    //         console.log(e)
+    //     }
+    // }
+    //
+    // reply = request()
+    //
+    //
+    // useEffect(() => {
+    //     request().then(response =>{
+    //         setActive(true)
+    //         for (let index in response){
+    //             if (response.hasOwnProperty(index)){
+    //                 // console.log(reply[index].name + '\n', 'longitude: ' + reply[index].longitude + '\n', 'latitude: ' + reply[index].latitude)
+    //                 localSettLementArray.push({name: response[index].name, longitude: response[index].longitude,latitude: response[index].latitude})
+    //             }
+    //         }
+    //         setLementArray(localSettLementArray)
+    //         // console.log('useEffect: ' + settLementArray)
+    //         // console.log(localSettLementArray[0].name)
+    //     })
+    // },[])
 
     // console.log(localSettLementArray)
     // console.log('NOT useEffect: ' + settLementArray)
 
     return<>
-        {active && localSettLementArray.map((pnt, index) => (
+        {active && localities.map((pnt, index) => (
             <Marker
+                icon={Icon}
                 key={index}
-                icon={GetIcon(10,10)}
                 position={new L.LatLng(pnt.longitude, pnt.latitude)}>
                 <Popup>{pnt.name}</Popup>
             </Marker>
         ))}
+
+
+        {/*{localities.map((loc, index) =>(*/}
+        {/*    <Marker*/}
+        {/*    key={index}*/}
+        {/*    position={new L.LatLng(loc.latitude, loc.longitude)}>*/}
+        {/*    </Marker>*/}
+        {/*))}*/}
     </>
 }
