c2d7f7c7d0e18ec7adba7394c910a83d8a978f7f
redis <rediskink@inbox.ru>
2023-05-08 15:44:43 +0700

    переделкин (Modal.jsx): - проверка по введенным данным на ошибку и наличие пдф. - стейт на разрешение открыть пдф. - после проверки кнопка разрешает перейти по созданному/уже имеющемуся. - алерты, на ошибочно введенные данные

--- src/components/MainNavBar/reportModal/Modal.jsx
+++ src/components/MainNavBar/reportModal/Modal.jsx
@@ -3,45 +3,8 @@ import React, {useContext, useState} from 'react';
 import {Context} from "../../Map/Context";
 import {URL_FOR_FILES} from "../../../config/config";
 import axios from "axios";
-import {value} from "lodash/seq";
-import {slidingWindow} from "react-horizontal-scrolling-menu";
-import {isEmpty} from "lodash";
-import {isRouteErrorResponse} from "react-router-dom";
 
-async function checkStates(pdfDateTime,pdfSubjectTag,cloudShielding,operatorFullName, url) {
-    if(!pdfDateTime || !pdfSubjectTag || !cloudShielding || !operatorFullName){
-        console.log('undef: ', pdfDateTime,pdfSubjectTag,cloudShielding,operatorFullName)
-        return false
-    }
-    else{
-        await axios.get(url).then(response => {
-            if(response.status === 200){
-                if(typeof response.data === 'object'){
-                    if(response.data.file_inf){
-                        console.log(response.data.file_inf)//данные введены верно, но данных нет
-                        return false
-                    }
-                    else if(response.data.fields_error){
-                        console.log(response.data.fields_error)//данные введены верно
-                        return false
-                    }
-                }
-                else{//если данные введены правильно и создан/есть отчет за выбранный период
-                    console.log("ready to next page", url)
-                    return true;
-                }
-            }
-            else{
-                console.warn('network error')
-                return false
-            }
-        }).catch(e => {
-            console.log(e.message);
-            return false
-        })
-    }
-    //window.open(URL,"_blank")
-}
+
 export function ModalReport ({active, setActive}){
 
     const subjectNames = [
@@ -68,94 +31,105 @@ export function ModalReport ({active, setActive}){
     const [pdfSubjectTag,setPdfSubjectTag] = useState()
     const [cloudShielding,setCloudShielding] = useState()
     const [operatorFullName,setOperatorFullName] = useState()
-    const [settlementsArray,setSettlementsArray] = useState()
-    const [settlementsIsActive,setsettlementsIsActive] = useState(false)
+    const [readyToTheNextPage,setReadyToTheNextPage] = useState(false)
     const URL = `${URL_FOR_FILES.URL_PDF}?date_time=${pdfDateTime}&cloud_shielding=${cloudShielding}&operator_fio=${operatorFullName}&subject_tag=${pdfSubjectTag}`
 
-    let requestReady = false
-    const requestForSettlements = () => {//запрос данных на массив id населенных пунктов
-        try{
-            axios.get(`${URL_FOR_FILES.URL_FOR_SETTLEMENTS}?date=${context.currentDate}&list_ids=${true}`).then(
-                async response =>{
-                    console.log(response.data['settlement_ids'].length)
-                    if(response.data['settlement_ids'].length > 0){
-                        await setSettlementsArray(response.data['settlement_ids'])
-                        await setsettlementsIsActive(true)
+
+    const checkStates = async () => {//проверка на наличие пдф по введенным данным
+        if(!pdfDateTime || !pdfSubjectTag || !cloudShielding || !operatorFullName){
+            alert("Проверьте введенные данные")
+            return false
+        }
+        else{
+            await axios.get(URL).then(response => {
+                if(response.status === 200){
+                    if(typeof response.data === 'object'){
+                        if(response.data.file_inf){
+                            alert(`Error: ${response.data.file_inf}\nОшибка: нет данных по вашему запросу`)//данные введены верно, но данных нет
+                            setReadyToTheNextPage(false)
+                        }
+                        else if(response.data.fields_error){
+                            console.log(`Error: ${response.data.fields_error}\nОшибка: данные введены неверно`)//данные введены верно
+                            setReadyToTheNextPage(false)
+                        }
+                    }
+                    else{//если данные введены правильно и создан/есть отчет за выбранный период
+                        console.log("ready to next page", URL)
+                        setReadyToTheNextPage(true)
                     }
                 }
-            )
-        }
-        catch (e){
-            console.log(e.message)
+                else{
+                    alert(`${response.status} network error`)
+                    setReadyToTheNextPage(false)
+                }
+            }).catch(e => {
+                console.log(e.message);
+                setReadyToTheNextPage(false)
+            })
         }
     }
 
-    const fioToGet = (e,value) => {
+    // const requestForSettlements = () => {//запрос данных на массив id населенных пунктов
+    //     try{
+    //         axios.get(`${URL_FOR_FILES.URL_FOR_SETTLEMENTS}?date=${context.currentDate}&list_ids=${true}`).then(
+    //             async response =>{
+    //                 console.log(response.data['settlement_ids'].length)
+    //                 if(response.data['settlement_ids'].length > 0){
+    //                     await setSettlementsArray(response.data['settlement_ids'])
+    //                     await setsettlementsIsActive(true)
+    //                 }
+    //             }
+    //         )
+    //     }
+    //     catch (e){
+    //         console.log(e.message)
+    //     }
+    // }
+
+    const fioToGet = (e,value) => {//пробелы в ФИО заменяет на "+" работает при onChange
         let localArray = value.split('')
-        let fioResult;
         for(let cell in localArray){
             if(localArray[cell] === ' '){
                 localArray[cell] = `+`
             }
         }
         setOperatorFullName(localArray.join(''))
-        console.log(localArray.join(''))
     }
-    const checkStatess = async () => {
-        if(!pdfDateTime || !pdfSubjectTag || !cloudShielding || !operatorFullName){
-            console.log('undef: ', pdfDateTime,pdfSubjectTag,cloudShielding,operatorFullName)
-            return false
-        }
-        else{
-           await axios.get(URL).then(response => {
-                if(response.status === 200){
-                    if(typeof response.data === 'object'){
-                        if(response.data.file_inf){
-                            console.log(response.data.file_inf)//данные введены верно, но данных нет
-                            return false
-                        }
-                        else if(response.data.fields_error){
-                            console.log(response.data.fields_error)//данные введены верно
-                            return false
-                        }
-                    }
-                    else{//если данные введены правильно и создан/есть отчет за выбранный период
-                        console.log("ready to next page", URL)
-                        return true;
-                    }
-                }
-                else{
-                    console.warn('network error')
-                    return false
-                }
-            }).catch(e => {
-                console.log(e.message);
-                return false
-            })
-        }
-        //window.open(URL,"_blank")
+
+    const toThePdf = () => {//открывает URL в новой вкладке, если проверка прошла успешна
+        window.open(URL, "_blank")
     }
-        //`${URL_FOR_FILES.URL_PDF}?date_time=${'2022-11-12T12:15'}&subject_tag=${pdfSubjectTag}&cloud_shielding=${cloudShielding}&operator_fio=${operatorFullName}`
+
     return<>
         <div className={active ? `${modalStyle.modal} ${modalStyle.modal_active}` : modalStyle.modal} onClick={()=> setActive(false)}>
             <div className={active ? `${modalStyle.modal_content} ${modalStyle.modal_content_active}` : modalStyle.modal_content} onClick={e => e.stopPropagation()}>
-                    <div className={modalStyle.modal_div}>
+
+
+                <div className={modalStyle.modal_div}>
                         <label className={modalStyle.modal_label}>date_time</label>
 
                         <input  type={"datetime-local"} className={modalStyle.modal_input}
 
                                 value={pdfDateTime}
-                                onChange={(e) => setPdfDateTime(e.target.value)}/>
+                                onChange={(e) => {
+                                    setPdfDateTime(e.target.value)
+                                    setReadyToTheNextPage(false)
+                                }}/>
                     </div>
 
+
                     <div className={modalStyle.modal_div}>
+
                         <label className={modalStyle.modal_label}>subject_tag</label>
 
                         <input list={'browsers'} type={"text"} className={modalStyle.modal_input}
                                placeholder={'Введите название и выберите регион'}
                                value={pdfSubjectTag}
                                onClick={""}
-                               onChange={(e) => setPdfSubjectTag(e.target.value)}/>
+                               onChange={(e) => {
+                                   setPdfSubjectTag(e.target.value)
+                                   setReadyToTheNextPage(false)
+                               }}/>
 
                         <datalist id="browsers" >
                             {subjectNames.map((settlement,index)=>(<option key={index} value={settlement.tag}>{settlement.name}</option>)
@@ -163,23 +137,36 @@ export function ModalReport ({active, setActive}){
                         </datalist>
                     </div>
 
+
                     <div className={modalStyle.modal_div}>
+
                         <label className={modalStyle.modal_label}>cloud_shielding</label>
 
                         <input type={"number"} className={modalStyle.modal_input}
                                value={cloudShielding}
-                               onChange={(e) => setCloudShielding(e.target.value)}/>
+                               onChange={(e) => {
+                                   setCloudShielding(e.target.value)
+                                   setReadyToTheNextPage(false)
+                               }}/>
                     </div>
 
+
                     <div className={modalStyle.modal_div}>
+
                         <label className={modalStyle.modal_label}>operator_fio</label>
 
                         <input type={"text"} className={modalStyle.modal_input}
                                value={operatorFullName}
-                               onChange={(e) => fioToGet(e,e.target.value)}/>
+                               onChange={(e) => {
+                                   fioToGet(e, e.target.value)
+                                   setReadyToTheNextPage(false)
+                               }}/>
                     </div>
-                <button onClick={() => checkStates(pdfDateTime,pdfSubjectTag,cloudShielding,operatorFullName,URL).then(response => {window.open(URL,"_blank")})}>check states</button>
-                    <a className={modalStyle.modal_button} href={`${URL_FOR_FILES.URL_PDF}?date_time=${pdfDateTime}&cloud_shielding=${cloudShielding}&operator_fio=${operatorFullName}&subject_tag=${pdfSubjectTag}`} target={"_blank"} value={"Сохранить отчёт"}>сформировать</a>
+
+                {readyToTheNextPage ?
+                    <button className={modalStyle.modal_button} onClick={() => toThePdf()} >Открыть отчет</button>
+                    :
+                    <button className={modalStyle.modal_button} onClick={() => checkStates()}>Проверить наличие данных</button>}
             </div>
         </div>
     </>
